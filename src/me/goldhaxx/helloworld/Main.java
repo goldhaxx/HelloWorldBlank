50d25383a66243ed02abd49fe82a1af944b04380
goldhaxx <zwright7@gmail.com>
2019-02-21 22:55:04 -0800

    Adding all files after completing first version of command

--- src/me/goldhaxx/helloworld/Main.java
+++ src/me/goldhaxx/helloworld/Main.java
@@ -1,5 +1,6 @@
 package me.goldhaxx.helloworld;
 
+import me.goldhaxx.helloworld.me.goldhaxx.helloworld.commands.HelloCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin  {
@@ -7,6 +8,10 @@ public class Main extends JavaPlugin  {
     @Override
     public void onEnable(){
 
+        new HelloCommand(this);
+
     }
 
 }
+
+/*comment*/
--- src/me/goldhaxx/helloworld/me/goldhaxx/helloworld/commands/HelloCommand.java
+++ src/me/goldhaxx/helloworld/me/goldhaxx/helloworld/commands/HelloCommand.java
@@ -1,4 +1,37 @@
 package me.goldhaxx.helloworld.me.goldhaxx.helloworld.commands;
 
-public class HelloCommand {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import me.goldhaxx.helloworld.Main;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class HelloCommand implements CommandExecutor {
+
+    private Main plugin;
+
+    public HelloCommand(Main plugin){
+
+        this.plugin=plugin;
+        plugin.getCommand("hello").setExecutor(this);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Only players may execute this command!");
+            return true;
+        }
+
+        Player p = (Player) sender;
+
+        if (p.hasPermission("hello.use")){
+            p.sendMessage("hi!");
+            return true;
+        }
+
+        p.sendMessage("You don't have permission to execute this command!");
+
+        return false;
+    }
 }
